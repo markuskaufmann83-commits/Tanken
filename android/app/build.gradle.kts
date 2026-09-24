c4d6844c0666ae5095e18d4692616525a49c9f59
Markus <markus@local>
2026-09-24 20:59:05 +0200

    build(android): set targetSdk to 36 and bump versionCode to 2 for Play Console requirement

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,9 +10,9 @@ android {
     defaultConfig {
         applicationId = "de.spritradar.aut"
         minSdk = 26
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        targetSdk = 36
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
